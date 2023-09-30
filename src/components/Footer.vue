cdcdc9f02d7eb208cf8054b6baf60fc33fc33610
Darya Makeeva <dasha.makeeva.99@mail.ru>
2023-09-30 15:32:33 +0300

    initialization of the finished template

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer>
+    <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item" v-for="(link, index) in links" :key="index">
+          <router-link class=".navbar-link" :to="link.url">
+            {{ link.title }}
+          </router-link>
+        </li>
+      </ul>
+
+      <!-- link to course -->
+      <ul class="navbar-list navbar-list-footer">
+        <li class="navbar-item navbar-item-footer">Сделано Макеевой Дарьей</li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { links } from '@/_config.js'
+
+export default {
+  data() {
+    return {
+      links: links
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar-item-footer {
+  opacity: 0.6;
+  font-size: 14px;
+}
+
+.navbar-list-footer {
+  margin-top: 14px;
+}
+</style>
